6b77bd861d4de9e118a88c5b2e671c1adb9a0aba
Matcha <admin@mcc.wiki>
2025-03-04 22:01:09 +0800

    style:change title color

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -14,7 +14,7 @@ const handleWindowMaximise = async () => {
 
 <template>
   <div style="--wails-draggable:drag" class="h-20 shrink-0 px-8 text-lg text-stone-400 flex flex-row justify-between items-center gap-2" @dblclick.self="handleWindowMaximise">
-    <div class="text-2xl font-bold text-white select-none mr-auto">
+    <div class="text-2xl font-bold select-none mr-auto bg-gradient-to-tl from-slate-800 via-violet-500 to-zinc-400 bg-clip-text text-transparent">
       <span>{{ $t('window.title') }}</span>
       <ButtonVersion/>
     </div>
